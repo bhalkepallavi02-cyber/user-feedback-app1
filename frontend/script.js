924cc036bedf84ebf8f45cda8c09b33faa0e9a91
bhalkepallavi02-cyber <bhalkepallavi02@gmail.com>
2026-01-28 14:22:39 +0530

    Update script.js

--- frontend/script.js
+++ frontend/script.js
@@ -1,1 +1,18 @@
+document.getElementById("form").addEventListener("submit", function(e) {
+  e.preventDefault();
 
+  const name = document.getElementById("name").value;
+  const email = document.getElementById("email").value;
+  const feedback = document.getElementById("feedback").value;
+
+  fetch("http://EC2_PUBLIC_DNS_OR_ALB_DNS/submit", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify({ name, email, feedback })
+  })
+  .then(res => res.json())
+  .then(data => alert("Feedback submitted"))
+  .catch(err => console.error(err));
+});
