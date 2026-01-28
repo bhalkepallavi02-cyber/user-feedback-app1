8ae652cc61502794d5eae9036dd2c7b7ba244f75
bhalkepallavi02-cyber <bhalkepallavi02@gmail.com>
2026-01-28 14:25:46 +0530

    app.py

--- backend/app.py
+++ backend/app.py
@@ -1,1 +1,14 @@
+from flask import Flask, request
+import boto3, uuid
 
+app = Flask(_name_)
+table = boto3.resource('dynamodb').Table('feedback')
+
+@app.route('/submit', methods=['POST'])
+def submit():
+    data = request.json
+    data['id'] = str(uuid.uuid4())
+    table.put_item(Item=data)
+    return {"status": "success"}
+
+app.run(host='0.0.0.0', port=5000)
